b8fa5e843e071e2dbe378380fed31db6b541e5bb
NhatMinh <62105206+LTNhatMinh@users.noreply.github.com>
2025-02-27 18:56:07 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+while :; do
+    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xme63av7ws6am2sgxxwak -r community-pools.mysrv.cloud:10300 -m 5 -p rpc;
+    sleep 5;
+done
